098850441a4123d488867e787cb2f6785a607d8a
James Conlon <conlonj25@gmail.com>
2022-11-09 17:32:03 +0100

    corrected deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "Switching to branch master"
 git checkout main
 
 echo "Building App..."
-npm run Building
+npm run build
 
 echo "Deploying files to server..."
 scp -r build/* james@178.79.182.193:/var/www/178.79.182.193/
